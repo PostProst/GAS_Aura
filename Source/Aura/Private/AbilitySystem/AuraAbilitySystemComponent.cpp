3610133b73b23f3543c901f933ba1ca23b5e0ab9
PostProst <poskriptum6@gmail.com>
2024-01-14 18:23:26 +0400

    Broadcasting Effect Asset Tags through GE delegates

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -3,3 +3,19 @@
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 
+void UAuraAbilitySystemComponent::AbilityActorInfoSet()
+{
+	// bind our callbacks to the ASC delegates when InitAbilityActorInfo() is set on the Character class
+	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
+}
+
+void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
+                                                const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
+{
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+
+	// broadcast our delegate so the WidgetController (if bound) can respond with its callback
+	EffectAssetTags.Broadcast(TagContainer);
+	
+}
