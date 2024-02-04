fbd4d8c6acf9a1c022773a46818791bfd00f04b9
PostProst <poskriptum6@gmail.com>
2024-02-04 18:29:12 +0400

    Asset Manager to set Gameplay Tags from native

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -9,6 +9,10 @@ FAuraGameplayTags FAuraGameplayTags::GameplayTags;
 
 void FAuraGameplayTags::InitializeNativeGameplayTags()
 {
-	// adding Gameplay Tags to global Tag Manager from native code
-	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduced damage taken, improves Block Chance"));
+	/*	add a new Gameplay Tag to global Gameplay Tags Manager from native code
+	 *	store created Tag in local associated FGameplayTag variable ->
+	 *	on the only existing FAuraGameplayTags::GameplayTags instance of this class
+	 */ 
+	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduced damage taken, improves Block Chance"));
+	
 }
