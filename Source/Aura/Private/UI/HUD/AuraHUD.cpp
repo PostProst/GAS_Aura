c4f1bf28ddd337b21fdf0d85501c1226640b7762
PostProst <poskriptum6@gmail.com>
2024-02-15 20:33:13 +0400

    Constructing an Attribute Widget Controller

--- Source/Aura/Private/UI/HUD/AuraHUD.cpp
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -3,19 +3,17 @@
 
 #include "UI/HUD/AuraHUD.h"
 #include "UI/Widget/AuraUserWidget.h"
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
 #include "UI/WidgetController/OverlayWidgetController.h"
 
 UOverlayWidgetController* AAuraHUD::GetOverlayWidgetController(const FWidgetControllerParams& WCParams)
 {
-	if(OverlayWidgetController == nullptr)
-	{
-		OverlayWidgetController = NewObject<UOverlayWidgetController>(this, OverlayWidgetControllerClass);
-		OverlayWidgetController->SetWidgetControllerParams(WCParams);
-		OverlayWidgetController->BindCallbacksToDependencies();
+	return GetWidgetController(OverlayWidgetController, OverlayWidgetControllerClass, WCParams);
+}
 
-		return OverlayWidgetController;
-	}
-	return OverlayWidgetController;
+UAttributeMenuWidgetController* AAuraHUD::GetAttributeMenuWidgetController(const FWidgetControllerParams& WCParams)
+{
+	return GetWidgetController(AttributeMenuWidgetController, AttributeMenuWidgetControllerClass, WCParams);
 }
 
 void AAuraHUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS)
--- Source/Aura/Public/UI/HUD/AuraHUD.h
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -11,6 +11,7 @@ class UAbilitySystemComponent;
 class UAuraUserWidget;
 class UOverlayWidgetController;
 struct FWidgetControllerParams;
+class UAttributeMenuWidgetController;
 
 /**
  * 
@@ -21,11 +22,22 @@ class AURA_API AAuraHUD : public AHUD
 	GENERATED_BODY()
 	
 public:
-	
-	UPROPERTY()
-	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
+	// template function for creating (if not existing) and returning any type of Widget Controller
+	template <typename T>
+	FORCEINLINE T* GetWidgetController(TObjectPtr<T> WidgetController, TSubclassOf<T> WidgetControllerClass, const FWidgetControllerParams& WCParams)
+	{
+		if(WidgetController == nullptr)
+		{
+			WidgetController = NewObject<T>(this, WidgetControllerClass);
+			WidgetController->SetWidgetControllerParams(WCParams);
+			WidgetController->BindCallbacksToDependencies();
+		}
+		return WidgetController;
+	}
 
 	UOverlayWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
+	UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const FWidgetControllerParams& WCParams);
 
 	void InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS);
 
@@ -34,6 +46,11 @@ protected:
 
 	
 private:
+
+	// Overlay Widget
+
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
 	
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
@@ -43,4 +60,12 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<UOverlayWidgetController> OverlayWidgetControllerClass;
+
+	// AttributeMenuWidgetController
+	
+	UPROPERTY()
+	TObjectPtr<UAttributeMenuWidgetController> AttributeMenuWidgetController;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAttributeMenuWidgetController> AttributeMenuWidgetControllerClass;
 };
