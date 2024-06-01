3d81974b64091d1e20ed3fb5b5eea79ab2e24c61
PostProst <poskriptum6@gmail.com>
2024-06-01 20:00:19 +0400

    Added custom Aura log channel

--- Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -2,6 +2,7 @@
 
 
 #include "AbilitySystem/Data/AttributeInfo.h"
+#include "Aura/AuraLogChannels.h"
 
 FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& Tag, bool bLogNotFound) const
 {
@@ -15,7 +16,7 @@ FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& T
 	// Log an error if the Tag wasn't found and bLogNotFound is set to true
 	if (bLogNotFound)
 	{
-		UE_LOG(LogTemp, Error, TEXT("Can't find Infr for AttributeTag %s on AttributeInfro %s"), *Tag.ToString(), *GetNameSafe(this));
+		UE_LOG(LogAura, Error, TEXT("Can't find Info for AttributeTag %s on AttributeInfo %s"), *Tag.ToString(), *GetNameSafe(this));
 	}
 	// return an empty struct if the required Tag wasn't found
 	return FAuraAttributeInfo();
