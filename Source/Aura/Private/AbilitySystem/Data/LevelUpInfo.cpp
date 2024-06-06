efb32e98066960747fe9f871776ed5eec79a27eb
PostProst <poskriptum6@gmail.com>
2024-06-06 21:32:40 +0400

    LevelUpInfo data asset

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,25 @@
+// P.S. project
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 GivenXP)
+{
+	bool bSearching = true;
+	int32 Level = 1;
+	while (bSearching)
+	{
+		// LevelUpInformation[1] = info for level 1, 0th element in this array is just a placeholder and doesn't contain any valuable data
+		if (LevelUpInformation.Num() - 1 <= Level) return Level;
+	
+		if (GivenXP >= LevelUpInformation[Level].ExpRequirement)
+		{
+			Level++;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
