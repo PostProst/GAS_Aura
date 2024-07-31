cffbaf16a25d4a8a1ffe1d42f153669b2b155d0f
PostProst <poskriptum6@gmail.com>
2024-07-31 14:37:35 +0400

    Spawn particle system on click to move

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -7,6 +7,7 @@
 #include "GameplayTagContainer.h"
 #include "AuraPlayerController.generated.h"
 
+class UNiagaraSystem;
 class UDamageTextWidgetComponent;
 struct FInputActionValue;
 class UInputAction;
@@ -90,6 +91,9 @@ private:
 
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USplineComponent> MoveSpline;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UNiagaraSystem> ClickNiagaraSystem;
 	/* end Click to move */
 
 	UPROPERTY(EditDefaultsOnly, Category="UI")
