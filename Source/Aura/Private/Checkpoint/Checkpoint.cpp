57961d99b251be62980282b81bbd2add892e1ea7
PostProst <poskriptum6@gmail.com>
2024-10-07 19:42:02 +0400

    Checkpoint

---
+++ Source/Aura/Private/Checkpoint/Checkpoint.cpp
@@ -0,0 +1,48 @@
+// P.S. project
+
+
+#include "Checkpoint/Checkpoint.h"
+#include "Components/SphereComponent.h"
+
+ACheckpoint::ACheckpoint(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>("Checkpoint Mesh");
+	CheckpointMesh->SetupAttachment(RootComponent);
+	CheckpointMesh->SetCollisionEnabled(ECollisionEnabled::Type::QueryAndPhysics);
+	CheckpointMesh->SetCollisionResponseToAllChannels(ECR_Block);
+	
+	OverlapSphere = CreateDefaultSubobject<USphereComponent>("OverlapSphere");
+	OverlapSphere->SetupAttachment(CheckpointMesh);
+	OverlapSphere->SetGenerateOverlapEvents(true);
+	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::Type::QueryOnly);
+	OverlapSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void ACheckpoint::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnSphereOverlap);
+	
+}
+
+void ACheckpoint::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{	
+	if (OtherActor && OtherActor->ActorHasTag(FName("Player")))
+	{
+		HandleGlowEffect();
+	}
+}
+
+void ACheckpoint::HandleGlowEffect()
+{
+	// disable collision on the Sphere so we can't overlap with it more than once
+	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision); 
+	UMaterialInstanceDynamic* DynamicMI = UMaterialInstanceDynamic::Create(CheckpointMesh->GetMaterial(0), this);
+	CheckpointMesh->SetMaterial(0, DynamicMI);
+	CheckpointReached(DynamicMI);
+}
